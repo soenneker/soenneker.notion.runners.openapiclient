c573aabc8eb35a0df4027447904d84440dd1d9cc
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:27 -0500

    xml commenting

--- src/Soenneker.Notion.Runners.OpenApiClient/Constants.cs
+++ src/Soenneker.Notion.Runners.OpenApiClient/Constants.cs
@@ -1,6 +1,12 @@
 namespace Soenneker.Notion.Runners.OpenApiClient;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.Notion.OpenApiClient";
 }
